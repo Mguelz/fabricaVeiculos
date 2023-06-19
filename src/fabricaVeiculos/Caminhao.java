69ba444c7a3cb247e9275567069f1a8431919dc6
logonrmlocal <logonrmlocal@PA603MICRO11.fiap.com.br>
2023-06-19 15:05:28 -0300

    ajustando as variaveis e os metodos

--- src/fabricaVeiculos/Caminhao.java
+++ src/fabricaVeiculos/Caminhao.java
@@ -1,5 +1,7 @@
 package fabricaVeiculos;
 
+import java.util.Date;
+
 /**
  * 
  * @author Felipe
@@ -7,5 +9,63 @@ package fabricaVeiculos;
  *
  */
 public class Caminhao extends Veiculo {
+
+	public Caminhao(String marca, String cor, String modelo, Date anoDeFabrica, String tipoDeCombustivel,
+			int velocidadeAtual, int limiteMaximo, Motor motor, Roda roda, Transmissao transmissao) {
+
+		super(marca, cor, modelo, anoDeFabrica, tipoDeCombustivel, velocidadeAtual, limiteMaximo, motor, roda,
+				transmissao);
+	}
+
+	@Override
+	public boolean acelerar(int qtdParaAcelerar, boolean ligadoDesligado, int velocidadeAtual) {
+		int velocidadeDesejada = this.velocidadeAtual + qtdParaAcelerar;
+		verificarSeEstaLigado(ligadoDesligado);
+		if (ligadoDesligado == true && velocidadeDesejada <= this.limiteMaximo) {
+			this.velocidadeAtual = velocidadeDesejada;
+			System.out.println("");
+			System.out.println("Acelerando...");
+			System.out.println("Velocidade atual " + this.velocidadeAtual + "Km/h");
+		} else if (ligadoDesligado == false) {
+			System.out.println("Nao eh possivel acelerar, pois o Carro esta desligado");
+		} else if (velocidadeDesejada >= this.limiteMaximo) {
+			this.velocidadeAtual = this.limiteMaximo;
+			System.out.println("Nao eh possivel acelerar, pois a velocidade maxima é " + this.limiteMaximo);
+			System.out.println("Velocidade atual " + this.limiteMaximo + "Km/h");
+		}
+		return false;
+	}
+
+	@Override
+	public void frear(int qtdParaFrear, boolean ligadoDesligado, int velocidadeAtual) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mostrarDados() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean verificarSeEstaLigado(boolean valor) {
+		if (valor == true) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean desligar(boolean desligado, int velocidadeAtual, int limiteMaximo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ligar(boolean ligado, int velocidadeAtual, int limiteMaximo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- src/fabricaVeiculos/Moto.java
+++ src/fabricaVeiculos/Moto.java
@@ -1,5 +1,7 @@
 package fabricaVeiculos;
 
+import java.util.Date;
+
 /**
  * 
  * @author Felipe
@@ -7,5 +9,47 @@ package fabricaVeiculos;
  *
  */
 public class Moto extends Veiculo {
+
+	public Moto(String marca, String cor, String modelo, Date anoDeFabrica, String tipoDeCombustivel,
+			int velocidadeAtual, int limiteMaximo, Motor motor, Roda roda, Transmissao transmissao) {
+		super(marca, cor, modelo, anoDeFabrica, tipoDeCombustivel, velocidadeAtual, limiteMaximo, motor, roda, transmissao);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean acelerar(int qtdParaAcelerar, boolean ligadoDesligado, int velocidadeAtual) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean desligar(boolean desligado, int velocidadeAtual, int limiteMaximo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ligar(boolean ligado, int velocidadeAtual, int limiteMaximo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void frear(int qtdParaFrear, boolean ligadoDesligado, int velocidadeAtual) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mostrarDados() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean verificarSeEstaLigado(boolean valor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
